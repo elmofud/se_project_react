c9987d5e0100b9b7661982bf6040ff2b4bbeca2d
Tracey Lee <traceylg43@gmail.com>
2025-12-12 13:55:57 -0500

    feet: create watherOtion array for different weather skins

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,33 @@ export const coordinates = {
 };
 
 export const APIkey = "b7c782aaef42fca2764f49759ebad35d";
+
+export const weatheOption = [
+  {
+    day: true,
+    condition: "sunny",
+    url: new URL("../assets/day/sunny.png", import.meta.url),
+    href,
+  },
+
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("../assets/day/cloudy.png", import.meta.url),
+    href,
+  },
+
+  {
+    day: false,
+    condition: "sunny",
+    url: new URL("../assets/night/sunny.png", import.meta.url),
+    href,
+  },
+
+  {
+    day: false,
+    condition: "sunny",
+    url: new URL("../assets/night/sunny.png", import.meta.url),
+    href,
+  },
+];
--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -10,7 +10,13 @@ export const filterWeatherData = (data) => {
   result.city = data.name;
   result.temp = { F: data.main.temp };
   result.type = getWeatherType(result.temp.F);
+  result.condition = data.weather[0].main.tolowerCase();
+  result.isDay = isDay(data.sys, Date.now());
   return result;
+};
+
+const isDay = ({ sunrise, sunset }, now) => {
+  return sunrise * 1000 < now && now < sunset * 1000;
 };
 
 const getWeatherType = (temperature) => {
