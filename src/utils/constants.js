cc20284afc00b1fedc9d319bf0682989b2876a84
Tracey Lee <traceylg43@gmail.com>
2025-12-12 17:09:28 -0500

    factor:weatherOtion by creating two folders day and night hod the six weatherOption groups miss two

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const coordinates = {
 
 export const APIkey = "b7c782aaef42fca2764f49759ebad35d";
 
-export const weatheOption = [
+export const weatherOption = [
   {
     day: true,
     condition: "sunny",
@@ -29,8 +29,62 @@ export const weatheOption = [
 
   {
     day: false,
-    condition: "sunny",
+    condition: "cloudy",
     url: new URL("../assets/night/sunny.png", import.meta.url),
+    href,
+  },
+
+  {
+    day: true,
+    condition: "rain",
+    url: new URL("../assets/day/rain.png", import.meta.url),
+    href,
+  },
+
+  {
+    day: true,
+    condition: "snow",
+    url: new URL("../assets/day/snow.png", import.meta.url),
+    href,
+  },
+  {
+    day: false,
+    condition: "rain",
+    url: new URL("../assets/night/rain.png", import.meta.url),
+    href,
+  },
+
+  {
+    day: flase,
+    condition: "snow",
+    url: new URL("../assets/night/snow.png", import.meta.url),
+    href,
+  },
+
+  {
+    day: true,
+    condition: "fog",
+    url: new URL("../assets/day/fog.png", import.meta.url),
+    href,
+  },
+
+  {
+    day: true,
+    condition: "storm",
+    url: new URL("../assets/day/storm.png", import.meta.url),
+    href,
+  },
+
+  day: false,
+    condition: "fog",
+    url: new URL("../assets/night/fog.png", import.meta.url),
+    href,
+  },
+
+  {
+    day: false,
+    condition: "storm",
+    url: new URL("../assets/night/storm.png", import.meta.url),
     href,
   },
 ];
